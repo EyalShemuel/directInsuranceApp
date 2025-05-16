c9cc9c46685fa05c593820ffcb7757abfb16b8d0
EyalShemuel <70960250+EyalShemuel@users.noreply.github.com>
2025-05-16 15:44:30 +0300

    10

--- components/MiddleSection.tsx
+++ components/MiddleSection.tsx
@@ -1,13 +1,13 @@
 import Checkbox from 'expo-checkbox';
 import { Image } from 'expo-image';
-import React from 'react';
-
-import { View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
 import AccessibleTextInput from './accessibleInput';
 import AccessibleButton from './AccsesibleButton';
 import AccsesibleText from './AccsesibleText';
+import PopupMessage from './PopupMessage';
 
-import Footer from './Footer';
+
 
 interface MiddleSectionProps {
   screenType: 'welcome' | 'otp';
@@ -18,19 +18,71 @@ const MiddleSection: React.FC<MiddleSectionProps> = ({
   screenType,
   setScreenType,
 }) => {
+  type VariantType = 'background' | 'active' | 'transparent';
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [disableTheConfirme, setdisableTheConfirme] = useState(true);
+  const [mainButtonName, setMainButtonName] = useState('שילחו לי את הקוד ב-');
+  const [selectedMethod, setSelectedMethod] = useState('sms'); // שיטת שליחה נוכחית
+  const [showPopup, setShowPopup] = useState(false);
+  const [idNumber, setIdNumber] = useState(''); // מספר תעודת זהות
+  const [variantMethodtype, setVariantMethodtype] = useState<VariantType>('background'); // סוג הכפתור הנוכחי
+  const [isChecked, setIsChecked] = useState(false); // מצב הצ'קבוקס
+  const [hasIdError, setHasIdError] = useState(false); // האם יש שגיאה בתעודת הזהות
+  
+  const sendingMethodText = ['Email', 'Whatsapp', 'SMS'];
+  
+  const closePopup = () => {
+    setShowPopup(false);
+  };
+  
+  const handlePressForMethod = (method: string) => {
+    // עדכון שיטת השליחה הנוכחית
+    setSelectedMethod(method);
+    // עדכון הטקסט של הכפתור הראשי
+    setMainButtonName('שילחו לי את הקוד ב-' + method);
+    // שינוי המצב של הכפתור לפעיל
+    setVariantMethodtype('active');
+  };
+
+  // בדיקת תקינות תעודת זהות
+  const validateId = (id: string) => {
+    setIdNumber(id);
+    // בדיקה אם מספר תעודת הזהות שונה מ-123456789
+    setHasIdError(id !== '' && id !== '123456789');
+  };
+
+  useEffect(() => {
+    // הפעלת כפתור שלח לי את הקוד אם הוזן מספר תעודת זהות תקין והצ'קבוקס מסומן
+    if (idNumber.length >= 9 && !hasIdError && isChecked) {
+      setdisableTheConfirme(false);
+    } else {
+      setdisableTheConfirme(true);
+    }
+  }, [idNumber, hasIdError, isChecked]);
+
+  useEffect(() => {
+    // עדכון הטקסט של הכפתור הראשי
+    handlePressForMethod(selectedMethod);
+  }, [selectedMethod]);
+
   const onSendCode = () => {
     // פונקציה לשליחת הקוד
+    if (hasIdError) {
+      // הצגת פופאפ שגיאה אם יש בעיה בתעודת הזהות
+      setShowPopup(true);
+      return;
+    }
     setScreenType('otp');
     console.log('Sending code...');
   };
 
-   
   return (
     <>
       <View className="flex-1 items-center justify-center mt-9">
         <Image
           className="w-full h-full"
-          source={require('@/assets/images/welcome.png')}          
+          source={require('@/assets/images/welcome.png')}
           contentFit="contain"
           transition={1000}
         />
@@ -39,9 +91,10 @@ const MiddleSection: React.FC<MiddleSectionProps> = ({
       <View className="flex-row justify-between items-center">
         <AccessibleTextInput
           label={`תעודת זהות/דרכון`}
-          required={true}
-          errorMessage={`מספר לא תקין`}
-          hasError={false}
+          required={true}         
+          onChange={(e) => validateId(e.nativeEvent.text)}
+          errorMessage={`מספר לא תקין - מספר תקין הוא 123456789`}
+          hasError={hasIdError}
           className="w-1/2"
           labelClassName="text-text-secondary text-xxs mb-1.5 font-medium"
           inputClassName="text-text-secondary w-full border-b-2 border-text-secondary"
@@ -61,7 +114,6 @@ const MiddleSection: React.FC<MiddleSectionProps> = ({
             containerStyle={{
               margin: 0,
               padding: 0,
-              
             }}
           />
           <AccsesibleText
@@ -71,7 +123,7 @@ const MiddleSection: React.FC<MiddleSectionProps> = ({
             fontWeight="normal"
             accessibilityHint="אני מאשר/ת קבלת מידע פרסומי ממועדון ישיר"
             disabled={false}
-             className='flex-shrink-0 ml-1 mr-1'
+            className="flex-shrink-0 ml-1 mr-1"
           />
           <AccessibleButton
             onPress={() => {
@@ -93,12 +145,69 @@ const MiddleSection: React.FC<MiddleSectionProps> = ({
             fontWeight="normal"
             accessibilityHint="אני מאשר/ת קבלת מידע פרסומי ממועדון ישיר"
             disabled={false}
-            className='flex-shrink-0 ml-1 mr-1'
+            className="flex-shrink-0 ml-1 mr-1"
           />
-          <Checkbox className="ml-2" onValueChange={() => {}} />
+          <Checkbox 
+            className="ml-2" 
+            value={isChecked} 
+            onValueChange={(value) => setIsChecked(value)}
+            accessibilityLabel="אישור תנאי השימוש ומדיניות הפרטיות"
+          />
         </View>
       </View>
-      <Footer onPress={onSendCode} />
+      <View>
+      <AccsesibleText text='שלח לי את הקוד ב-' className="text-center justify-center self-center items-center" />
+
+      {/* אפשרויות השליחה */}
+      <View className='flex-row justify-center items-center self-center'>
+        {sendingMethodText.map((method, index) => (
+          <View key={index} className='flex-row items-center'>
+            <AccessibleButton
+              label={method}
+              variant={method === selectedMethod ? 'active' : 'transparent'}
+              disabled={isLoading}
+              onPress={() => handlePressForMethod(method)}
+              accessibilityLabel={`בחר ${method}`}
+              accessibilityHint={`שלח לי את הקוד ב-${method}`}
+              textColor='#404156'
+              activeColor={method === selectedMethod ? '#388E3C' : '#404156'}
+              // אם הכפתור נבחר, הטקסט יודגש
+              textStyle={{ fontWeight: method === selectedMethod ? 'bold' : 'normal' }}
+              containerStyle={{ marginHorizontal: 5, padding: 10 }}
+            />
+
+            {/* מפריד בין האפשרויות - למעט האחרון */}
+            {index < sendingMethodText.length - 1 && (
+              <Text className='text-lg mx-3 text-text-secondary'> | </Text>
+            )}
+          </View>
+        ))}
+      </View>
+
+      <View >
+        <AccessibleButton
+          label={mainButtonName}
+          variant='background'
+          disabled={disableTheConfirme}
+          onPress={onSendCode}
+          
+          accessibilityLabel={mainButtonName}
+          accessibilityHint={`שלח לי את הקוד ב-${selectedMethod}`}
+          textColor='#FFFFFF'
+          activeColor='#388E3C'
+          containerStyle={{ marginTop: 20, marginBottom: 20, borderRadius: 25, width: 250, height: 40, justifyContent: 'center', alignSelf: 'center' }}
+
+        />
+      </View>
+
+      <PopupMessage
+        header={'שגיאה!'}
+        text={'לא הצלחנו לאמת את פרטי הכניסה\n שלך. נשמח לדבר ב WhatsApp\n ולעזור לך במהירות.'}
+        visible={showPopup}
+        onConfirm={closePopup}
+        onClose={closePopup}
+      />
+    </View>
     </>
   );
 };
