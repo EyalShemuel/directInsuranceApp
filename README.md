af2c706ffa69822bff8eee707738b5e3a88a3ffc
EyalShemuel <70960250+EyalShemuel@users.noreply.github.com>
2025-05-16 17:23:55 +0300

    12

--- README.md
+++ README.md
@@ -1,19 +1,29 @@
 This is an Riact Native with EXPO , typeScript and NativeWind
 
+
+
+The is an application that simulates login screens as asked in the attached exercise.
+The application was built in React Native with EXPO and NativeWind while maintaining accessibility principles. In accordance with WACAG2.0 as required by regulations.
+
+Making it accessible required changing the color of a checkbox, expanding buttons for easier touch, adding ALT to images, using buttons and text customized for accessibility, and more...
+
 ## Get started
 
-1. Install dependencies
+1. clone the rposetory and do to the riposetory derectory
 
-   ```bash
-   npm install
+2. Install dependencies  
+   type: npm install
 
-   you may need to install - lightningcss-win32-x64-msvc
+3. Start the app
+   type: npx expo start
+
+4. scan the QR code on the screan to view on your devaice
+
+
+
+
+
+Troubleshooting
+
+you may need to install - lightningcss-win32-x64-msvc
    by: npm i lightningcss-win32-x64-msvc
-   ```
-
-2. Start the app
-
-   ```bash
-   npx expo start
-   ```
-
--- components/MiddleSection.tsx
+++ components/MiddleSection.tsx
@@ -7,8 +7,6 @@ import AccessibleButton from './AccsesibleButton';
 import AccsesibleText from './AccsesibleText';
 import PopupMessage from './PopupMessage';
 
-
-
 interface MiddleSectionProps {
   screenType: 'welcome' | 'otp';
   setScreenType: React.Dispatch<React.SetStateAction<'welcome' | 'otp'>>;
@@ -28,32 +26,34 @@ const MiddleSection: React.FC<MiddleSectionProps> = ({
   // const [variantMethodtype, setVariantMethodtype] = useState<VariantType>('background'); // סוג הכפתור הנוכחי
   const [isChecked, setIsChecked] = useState(false); // מצב הצ'קבוקס
   const [hasIdError, setHasIdError] = useState(false); // האם יש שגיאה בתעודת הזהות
-  
+
   const sendingMethodText = ['Email', 'Whatsapp', 'SMS'];
 
   const theCorectIdNumber = '123456789'; // מספר תעודת זהות תקין
   const ERROR_MESSAGE = 'מספר תעודת זהות לא תקין - מספר תקין הוא 123456789';
-  
+
+  const checkboxId = React.useId();
+
   const closePopup = () => {
     setShowPopup(false);
   };
-  
+
   // פונקציה לבחירת שיטת שליחה SMS, Email, Whatsapp
-  const handlePressForMethod = (method: string) => {  
-    setSelectedMethod(method);   
-    setMainButtonName('שילחו לי את הקוד ב-' + method);   
+  const handlePressForMethod = (method: string) => {
+    setSelectedMethod(method);
+    setMainButtonName('שילחו לי את הקוד ב-' + method);
     // setVariantMethodtype('active');
   };
 
   // בדיקת תקינות תעודת זהות
   const validateId = (id: string) => {
-    setIdNumber(id);    
+    setIdNumber(id);
     setHasIdError(id !== '' && id !== theCorectIdNumber);
   };
 
   useEffect(() => {
     // הפעלת כפתור שלח לי את הקוד אם הוזן מספר תעודת זהות תקין והצ'קבוקס מסומן
-    if (idNumber.length >= 9 &&  isChecked) {
+    if (idNumber.length >= 9 && isChecked) {
       setdisableTheConfirme(false);
     } else {
       setdisableTheConfirme(true);
@@ -84,13 +84,15 @@ const MiddleSection: React.FC<MiddleSectionProps> = ({
           source={require('@/assets/images/welcome.png')}
           contentFit="contain"
           transition={1000}
+          accessibilityLabel="תמונה של אישה מחייכת"
+          accessibilityHint="תמונה של אישה מחייכת"
         />
       </View>
 
       <View className="flex-row justify-between items-center">
         <AccessibleTextInput
           label={`תעודת זהות/דרכון`}
-          required={true}         
+          required={true}
           onChange={(e) => validateId(e.nativeEvent.text)}
           errorMessage={ERROR_MESSAGE}
           hasError={hasIdError}
@@ -101,112 +103,145 @@ const MiddleSection: React.FC<MiddleSectionProps> = ({
       </View>
 
       <View className="flex flex-row items-center justify-end w-full">
-        <View className="flex flex-row items-center mb-7">
-          <AccessibleButton
-            onPress={() => {
-              console.log('מדיניות פרטיות');
-            }}
-            label="מדיניות הפרטיות"
-            variant="transparent"
-            textColor="#7C4DFF"
-            className="whitespace-nowrap flex-shrink-0"
-            containerStyle={{
-              margin: 0,
-              padding: 0,
-            }}
-          />
-          <AccsesibleText
-            text="ואת"
-            type="text"
-            size="small"
-            fontWeight="normal"
-            accessibilityHint="אני מאשר/ת קבלת מידע פרסומי ממועדון ישיר"
-            disabled={false}
-            className="flex-shrink-0 ml-1 mr-1"
-          />
-          <AccessibleButton
-            onPress={() => {
-              console.log('תנאי שימוש');
-            }}
-            label="תנאי השימוש"
-            variant="transparent"
-            textColor="#7C4DFF"
-            className="whitespace-nowrap flex-shrink-0"
-            containerStyle={{
-              margin: 0,
-              padding: 0,
-            }}
-          />
-          <AccsesibleText
-            text="אישרתי את"
-            type="text"
-            size="small"
-            fontWeight="normal"
-            accessibilityHint="אני מאשר/ת קבלת מידע פרסומי ממועדון ישיר"
-            disabled={false}
-            className="flex-shrink-0 ml-1 mr-1"
-          />
-          <Checkbox 
-            className="ml-2" 
-            value={isChecked} 
+        {/* צ'ק בוס אישור מדיניות - לא תקין צריך שכתוב מחדש */}
+        <View
+          className="flex flex-row-reverse items-center mb-7"
+          accessibilityRole="none"
+        >
+          <Checkbox
+            className="ml-2"
+            value={isChecked}
             onValueChange={(value) => setIsChecked(value)}
             accessibilityLabel="אישור תנאי השימוש ומדיניות הפרטיות"
+            color={isChecked ? '#7C4DFF' : undefined}
+            accessibilityRole="checkbox"
+            accessibilityState={{ checked: isChecked }}
           />
-        </View>
-      </View>
-      <View>
-      <AccsesibleText text='שלח לי את הקוד ב-' className="text-center justify-center self-center items-center" />
 
-      {/* אפשרויות השליחה */}
-      <View className='flex-row justify-center items-center self-center'>
-        {sendingMethodText.map((method, index) => (
-          <View key={index} className='flex-row items-center'>
-            <AccessibleButton
-              label={method}
-              variant={method === selectedMethod ? 'active' : 'transparent'}
-              
-              onPress={() => handlePressForMethod(method)}
-              accessibilityLabel={`בחר ${method}`}
-              accessibilityHint={`שלח לי את הקוד ב-${method}`}
-              textColor='#404156'
-              activeColor={method === selectedMethod ? '#388E3C' : '#404156'}
-              // אם הכפתור נבחר, הטקסט יודגש
-              textStyle={{ fontWeight: method === selectedMethod ? 'bold' : 'normal' }}
-              containerStyle={{ marginHorizontal: 5, padding: 10 }}
+          {/* שימוש בקבוצה נגישה שתייצג את כל משפט האישור */}
+          <View
+            accessibilityLabel="אישרתי את תנאי השימוש ואת מדיניות הפרטיות"
+            className=" items-center flex-row-reverse"
+            nativeID={checkboxId}
+          >
+            <AccsesibleText
+              text="אישרתי את"
+              type="text"
+              size="small"
+              fontWeight="normal"
+              accessibilityHint="חלק ממשפט האישור"
+              disabled={false}
+              className="flex-shrink-0 ml-1 mr-1"
             />
 
-            {/* מפריד בין האפשרויות - למעט האחרון */}
-            {index < sendingMethodText.length - 1 && (
-              <Text className='text-lg mx-3 text-text-secondary'> | </Text>
-            )}
+            <AccessibleButton
+              onPress={() => {
+                console.log('תנאי שימוש');
+              }}
+              label="תנאי השימוש"
+              variant="transparent"
+              textColor="#7C4DFF"
+              className="whitespace-nowrap flex-shrink-0"
+              containerStyle={{
+                margin: 0,
+                padding: 0,
+              }}
+              accessibilityHint="פתיחת עמוד תנאי השימוש"
+            />
+
+            <AccsesibleText
+              text="ואת"
+              type="text"
+              size="small"
+              fontWeight="normal"
+              accessibilityHint="חלק ממשפט האישור"
+              disabled={false}
+              className="flex-shrink-0 ml-1 mr-1"
+            />
+
+            <AccessibleButton
+              onPress={() => {
+                console.log('מדיניות פרטיות');
+              }}
+              label="מדיניות הפרטיות"
+              variant="transparent"
+              textColor="#7C4DFF"
+              className="whitespace-nowrap flex-shrink-0"
+              containerStyle={{
+                margin: 0,
+                padding: 0,
+              }}
+              accessibilityHint="פתיחת עמוד מדיניות הפרטיות"
+            />
           </View>
-        ))}
+        </View>
       </View>
 
-      <View >
-        <AccessibleButton
-          label={mainButtonName}
-          variant='background'
-          disabled={disableTheConfirme}
-          onPress={onSendCode}
-          
-          accessibilityLabel={mainButtonName}
-          accessibilityHint={`שלח לי את הקוד ב-${selectedMethod}`}
-          textColor='#FFFFFF'
-          activeColor='#388E3C'
-          containerStyle={{ marginTop: 20, marginBottom: 20, borderRadius: 25, width: 250, height: 40, justifyContent: 'center', alignSelf: 'center' }}
+      <View>
+        <AccsesibleText
+          text="שלח לי את הקוד ב-"
+          className="text-center justify-center self-center items-center"
+        />
 
+        {/* אפשרויות השליחה */}
+        <View className="flex-row justify-center items-center self-center">
+          {sendingMethodText.map((method, index) => (
+            <View key={index} className="flex-row items-center">
+              <AccessibleButton
+                label={method}
+                variant={method === selectedMethod ? 'active' : 'transparent'}
+                onPress={() => handlePressForMethod(method)}
+                accessibilityLabel={`בחר ${method}`}
+                accessibilityHint={`שלח לי את הקוד ב-${method}`}
+                textColor="#404156"
+                activeColor={method === selectedMethod ? '#388E3C' : '#404156'}
+                // אם הכפתור נבחר, הטקסט יודגש
+                textStyle={{
+                  fontWeight: method === selectedMethod ? 'bold' : 'normal',
+                }}
+                containerStyle={{ marginHorizontal: 5, padding: 10 }}
+              />
+
+              {/* מפריד בין האפשרויות - למעט האחרון */}
+              {index < sendingMethodText.length - 1 && (
+                <Text className="text-lg mx-3 text-text-secondary"> | </Text>
+              )}
+            </View>
+          ))}
+        </View>
+
+        <View>
+          <AccessibleButton
+            label={mainButtonName}
+            variant="background"
+            disabled={disableTheConfirme}
+            onPress={onSendCode}
+            accessibilityLabel={mainButtonName}
+            accessibilityHint={`שלח לי את הקוד ב-${selectedMethod}`}
+            textColor="#FFFFFF"
+            activeColor="#388E3C"
+            containerStyle={{
+              marginTop: 20,
+              marginBottom: 20,
+              borderRadius: 25,
+              width: 250,
+              height: 40,
+              justifyContent: 'center',
+              alignSelf: 'center',
+            }}
+          />
+        </View>
+
+        <PopupMessage
+          header={'שגיאה!'}
+          text={
+            'לא הצלחנו לאמת את פרטי הכניסה\n שלך. נשמח לדבר ב WhatsApp\n ולעזור לך במהירות.'
+          }
+          visible={showPopup}
+          onConfirm={closePopup}
+          onClose={closePopup}
         />
       </View>
-
-      <PopupMessage
-        header={'שגיאה!'}
-        text={'לא הצלחנו לאמת את פרטי הכניסה\n שלך. נשמח לדבר ב WhatsApp\n ולעזור לך במהירות.'}
-        visible={showPopup}
-        onConfirm={closePopup}
-        onClose={closePopup}
-      />
-    </View>
     </>
   );
 };
